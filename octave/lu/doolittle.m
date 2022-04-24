47847cbc4101b4212f5f9da219b1d7bfdb6f8347
Anton Puiu <anton.puiu@email.com>
2022-04-24 21:58:00 +0300

    update of j counter.

--- octave/lu/doolittle.m
+++ octave/lu/doolittle.m
@@ -4,17 +4,12 @@ function [L, U] = doolittle(A = magic(3))
   U = zeros(m, n);
 
   for i = 1:n
-    for j = 1:n
+    for j = i:n
       U(i, j) = A(i, j) - L(i, 1:i-1) * U(1:i-1, j);
 
-      if i != j
+      if j > i
         L(j, i) = 1/U(i, i) * (A(j, i) - L(j, 1:i-1) * U(1:i-1, i));
       endif
     endfor
-  endfor
-
-  for i = 1:n-1
-    cols = i+1:n;
-    L(i, cols) = 0;
   endfor
 endfunction
