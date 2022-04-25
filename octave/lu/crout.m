c588d0727f78e0c7f6464b1b542c2611f7729e8f
Anton Puiu <anton.puiu@email.com>
2022-04-25 05:01:35 +0300

    vectorized.

--- octave/lu/crout.m
+++ octave/lu/crout.m
@@ -1,6 +1,6 @@
 function [L, U] = crout(A = [2 -1 3; 4 5 1; 2 1 2])
   % t = time(); [l,u]=crout(magic(500)); disp(time() - t)
-  % 9.7470
+  % 0.2835
   [m, n] = size(A);
   L = zeros(n, n);
   U = eye(n);
@@ -10,14 +10,14 @@ function [L, U] = crout(A = [2 -1 3; 4 5 1; 2 1 2])
   end
 
   for j = 1:n
-    ## L(j:n, j) = A(j:n, j) - L(j:n, 1:j-1) * U(1:j-1, j);
-    for i = j:n
-      L(i, j) = A(i, j) - L(i,1:j-1) * U(1:j-1, j);
-    end
+    L(j:n, j) = A(j:n, j) - L(j:n, 1:j-1) * U(1:j-1, j);
+    ## for i = j:n
+    ##   L(i, j) = A(i, j) - L(i,1:j-1) * U(1:j-1, j);
+    ## end
 
-    ## U(j, j:n) = 1 / L(j, j) * (A(j, j:n) - L(j, 1:j-1) * U(1:j-1, j:n));
-    for i = j+1:n
-      U(j, i) = 1 / L(j, j) * (A(j, i) - L(j, 1:j-1) * U(1:j-1, i));
-    end
+    U(j, j:n) = 1 / L(j, j) * (A(j, j:n) - L(j, 1:j-1) * U(1:j-1, j:n));
+    ## for i = j+1:n
+    ##   U(j, i) = 1 / L(j, j) * (A(j, i) - L(j, 1:j-1) * U(1:j-1, i));
+    ## end
   end
 end
