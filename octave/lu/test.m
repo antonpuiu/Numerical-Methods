689005234e01da1681ca33590627d22f45db6e58
Anton Puiu <anton.puiu@email.com>
2022-04-26 03:50:37 +0300

    inserted octave graph

--- octave/lu/test.m
+++ octave/lu/test.m
@@ -3,6 +3,16 @@ function test()
   t_normal = zeros(1, length(interval));
   t_opt = zeros(1, length(interval));
   t_fast = zeros(1, length(interval));
+  t_super = zeros(1, length(interval));
+
+  for i = interval
+    printf("fourth loop:i = %d\n", i);
+    A = magic(i);
+    [Q, _] = qr(A);
+
+    t = time(); lu(Q); t_super(i+1) = time() - t;
+  end
+
 
   for i = interval
     printf("third loop:i = %d\n", i);
@@ -34,6 +44,8 @@ function test()
   plot(interval, t_opt, ";opt;");
   hold on;
   plot(interval, t_fast, ";fast;");
+  hold on;
+  plot(interval, t_super, ";octave-lu;");
   title("LU computation time over dimension");
   legend("show");
   xlabel("Dimension");
