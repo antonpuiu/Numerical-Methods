b03a8fc002f90e1ae677943a42d866446bd82197
Puiu Anton <anton.puiu@email.com>
2022-05-01 15:30:14 +0300

    restructured repository.

--- lu/test.m
+++ lu/test.m
@@ -6,48 +6,47 @@ function test()
   t_super = zeros(1, length(interval));
 
   for i = interval
-    printf("fourth loop:i = %d\n", i);
     A = magic(i);
-    [Q, _] = qr(A);
-
+    [Q, R] = qr(A);
+    
     t = time(); lu(Q); t_super(i+1) = time() - t;
   end
 
 
   for i = interval
-    printf("third loop:i = %d\n", i);
     A = magic(i);
-    [Q, _] = qr(A);
+    [Q, R] = qr(A);
 
     t = time(); doolittle(Q); t_fast(i+1) = time() - t;
   end
 
-  for i = interval
-    printf("second loop:i = %d\n", i);
-    A = magic(i);
-    [Q, _] = qr(A);
+  % for i = interval
+  %   printf("second loop:i = %d\n", i);
+  %   A = magic(i);
+  %   [Q, _] = qr(A);
 
-    t = time(); doolittle_opt(Q); t_opt(i+1) = time() - t;
-  end
+  %   t = time(); doolittle_opt(Q); t_opt(i+1) = time() - t;
+  % end
 
-  for i = interval
-    printf("first loop:i = %d\n", i);
-    A = magic(i);
-    [Q, _] = qr(A);
+  % for i = interval
+  %   printf("first loop:i = %d\n", i);
+  %   A = magic(i);
+  %   [Q, _] = qr(A);
 
-    t = time(); doolittle_normal(Q); t_normal(i+1) = time() - t;
-  end
+  %   t = time(); doolittle_normal(Q); t_normal(i+1) = time() - t;
+  % end
 
   figure;
-  plot(interval, t_normal, ";normal;");
-  hold on;
-  plot(interval, t_opt, ";opt;");
-  hold on;
+  % plot(interval, t_normal, ";normal;");
+  % hold on;
+  % plot(interval, t_opt, ";opt;");
+  % hold on;
   plot(interval, t_fast, ";fast;");
   hold on;
   plot(interval, t_super, ";octave-lu;");
   title("LU computation time over dimension");
   legend("show");
   xlabel("Dimension");
-  ylabel("Time[s]")
+  ylabel("Time[s]");
+
 end
